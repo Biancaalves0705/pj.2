9e123c1f9dd49a639c98f734cee848cc18bb3530
Biancaalves0705 <117362835+Biancaalves0705@users.noreply.github.com>
2024-05-06 08:25:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,6 @@
 <body>
     <section class="conteudo">
     
-   
-    <!-- Código omitido -->
-    
-    </section>
-    <script src="main.js"></script>
-</body>
-const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 12;
-numeroSenha.textContent = tamanhoSenha;
-
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-
-botoes[0].onclick = diminuiTamanho;
-
-function diminuiTamanho(){ <body>
-    <section class="conteudo">
-    
-   
-    <!-- Código omitido -->
     
     </section>
     <script src="main.js"></script>
@@ -49,6 +30,52 @@ function aumentaTamanho() {
     }
     numeroSenha.textContent = tamanhoSenha;
 }
+
+
+const campoSenha = document.querySelector('#campo-senha');
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();
+
+function geraSenha(){
+ for (let i = 0; i < tamanhoSenha;i++) {
+     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+     numeroAleatorio = Math.floor(numeroAleatorio);
+     console.log(letrasMaiusculas[numeroAleatorio]);
+ }
+}
+
+campoSenha.value = letrasMaiusculas;
+
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+
+function geraSenha(){
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){<body>
     tamanhoSenha = tamanhoSenha - 1;
 }
 
@@ -113,3 +140,40 @@ function geraSenha(){
     }
     campoSenha.value = senha;
     }
+
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    console.log(alfabeto);
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*alfabeto.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+}
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox');
+const forcaSenha = document.querySelector('forca');
+
+function classificaSenha(){
+    forcaSenha.classList.remove('fraca','media','forte');
+        if (tamanhoSenha > 11){
+           forcaSenha.classList.add('forte');
+        } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+            forcaSenha.classList.add('media');
+        } else if (tamanhoSenha <= 5){
+            forcaSenha.classList.add('fraca');
+        }
+}   
